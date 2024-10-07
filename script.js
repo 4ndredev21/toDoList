ea5c2a5b0cbccbd71237c8e9114bc20378160dce
4ndredev21 <4ndredev21@gmail.com>
2024-10-07 05:49:04 -0300

    finalizado o TODOLIST

--- script.js
+++ script.js
@@ -2,14 +2,31 @@ const tasklist = document.getElementById("taskList");
 const taskInput = document.getElementById("taskInput");
 
 function addTask() {
-  const taskText = taskInput.ariaValueMax.trim();
-  if (taskText != "") {
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const maxText = taskText.substring(0, 35);
     const li = document.createElement("li");
     li.innerHTML = `
-    <span>$(taskText)</span>
+    <span>${maxText}</span>
     <button class="editButton" onClick="editTask(this)">Editar</button>
-    <button class="editButton" onClick="editTask(this)">Editar</button>
-    <button>Remover</button>
+    <button class="deleteButton" onClick="deleteTask(this)">Remover</button>
+   
     `;
+    tasklist.appendChild(li);
+    taskInput.value = "";
   }
+}
+
+function editTask(button) {
+  const li = button.parentElement;
+  const span = li.querySelector("span");
+  const newText = prompt("Editar tarefa", span.textContent);
+  if (newText !== null && newText.trim() !== "") {
+    span.textContent = newText.trim();
+  }
+}
+
+function deleteTask(button) {
+  const li = button.parentElement;
+  tasklist.removeChild(li);
 }
